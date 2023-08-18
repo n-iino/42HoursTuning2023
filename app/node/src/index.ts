41d226e301ca74217a565fefa0220fce2525f54b
n-iino <n_iino@dreamarts.co.jp>
2023-08-18 11:56:58 +0900

    feat: add datadog

--- app/node/src/index.ts
+++ app/node/src/index.ts
@@ -1,4 +1,13 @@
+import tracer from "dd-trace";
 import { app } from "./app";
+
+tracer.init({
+  hostname: "datadog-agent",
+  port: 8126,
+  env: "local",
+  logInjection: true,
+  profiling: true,
+});
 
 const port = 8000;
 app.listen(port, () => {
