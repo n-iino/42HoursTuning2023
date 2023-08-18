ceb40c941027e763bf34e3c646ab26b3c6905cf4
YunosukeY <y_yamada@dreamarts.co.jp>
2023-08-18 17:31:38 +0900

    スロークエリログを無効化

--- app/mysql/mysql_migration/0_sample.sql
+++ app/mysql/mysql_migration/0_sample.sql
@@ -1,6 +1,3 @@
-set global slow_query_log=1;
-set global long_query_time=1;
-
 create index user_id_index on match_group_member(user_id);
 create index department_role_member_user_id_belong on department_role_member(user_id, belong);
 create index role_id_and_belong_idx on department_role_member(role_id, belong);
