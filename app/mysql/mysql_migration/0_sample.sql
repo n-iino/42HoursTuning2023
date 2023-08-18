6d759ddf139b6aa77378abac9470eec37ff2632d
YunosukeY <y_yamada@dreamarts.co.jp>
2023-08-18 13:23:29 +0900

    department_role_memberのuser_id, belongにインデックス追加

--- app/mysql/mysql_migration/0_sample.sql
+++ app/mysql/mysql_migration/0_sample.sql
@@ -2,3 +2,4 @@ set global slow_query_log=1;
 set global long_query_time=1;
 
 create index user_id_index on match_group_member(user_id);
+create index department_role_member_user_id_belong on department_role_member(user_id, belong);
