62d3d1a3f8669d0c04e10fc35c31d327bf3faac4
toushobun <so_deng@dreamarts.co.jp>
2023-08-18 15:32:24 +0900

    add index(mail, password) to user

--- app/mysql/mysql_migration/0_sample.sql
+++ app/mysql/mysql_migration/0_sample.sql
@@ -4,3 +4,4 @@ set global long_query_time=1;
 create index user_id_index on match_group_member(user_id);
 create index department_role_member_user_id_belong on department_role_member(user_id, belong);
 create index role_id_and_belong_idx on department_role_member(role_id, belong);
+create index mail_and_password_idx on user(mail, password);
